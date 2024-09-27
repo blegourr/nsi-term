0b1ad8fbad760d4bf64c3c5a96877fb96f8b1057
Blegourr <91756178+blegourr@users.noreply.github.com>
2024-09-27 07:15:03 +0000

    finish first reel Eval

---
+++ eval/1/ex1.py
@@ -0,0 +1,18 @@
+# ------------------------------
+#            FUNCTION
+# ------------------------------
+def indice_du_min(tab:list) -> int: # initialisation de la function
+  min = None
+  for value in tab: 
+    if min == None or (value < min): # si valeur min non définie ou value < min définie le nouveau min
+      min = value
+  
+  for i in range(len(tab)): # récupère la première value du tab avec la valeur min (ou inférieur)
+    if (tab[i] <= min): return i # renvoie l'index min
+
+# ------------------------------
+#               TEST
+# ------------------------------
+print(indice_du_min([5])) # -> 0
+print(indice_du_min([2,4,1])) # -> 2
+print(indice_du_min([5, 3, 2, 2, 4])) # -> 2
