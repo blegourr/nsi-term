927af727b8c420e14289cb43d7a552839ea4e84a
Blegourr <91756178+blegourr@users.noreply.github.com>
2024-10-07 11:56:47 +0000

    exo chap3

--- chap3/ex.py
+++ chap3/ex.py
@@ -1,6 +1,42 @@
+"""----------------- ex 1 -----------------"""
 class Carte:
     def __init__(self, number, signe):
         self.number = number
         self.signe = signe
 
+"""----------------- ex 2 -----------------"""
+class Voiture:
+    def __init__(self, couleur, vitesse) -> None:
+        self.couleur = couleur
+        self.vitesse = vitesse
+
+voit_jonathan = Voiture("rouge", 150)
+voit_denis = Voiture("bleu", 150)
+# print(voit_jonathan.couleur)
+# print(voit_denis.couleur)
+
+"""----------------- ex 3 -----------------"""
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f"Point({self.x}, {self.y})"
     
+    def deplace(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def abscisse(self) -> int:
+        return self.x
+
+    
+    def ordonnee(self) -> int:
+        return self.y
+
+a = Point(2, 4)
+print(a)
+print(a.abscisse())
+
+
