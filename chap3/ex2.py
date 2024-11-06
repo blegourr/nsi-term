6869cfca726ca56d5d02116a9dae3033d57ba568
Blegourr <91756178+blegourr@users.noreply.github.com>
2024-11-06 10:28:02 +0000

    update

--- chap3/ex2.py
+++ chap3/ex2.py
@@ -89,3 +89,67 @@ class Date():
     return dateA < dateB
   
 """----------------- ex 3 -----------------"""
+class Tableau():
+  def __init__(self, i_min, i_max, v) -> None:
+    self.i_min = i_min
+    self.i_max = i_max
+    self.v = v
+    tab = []
+    
+    for _ in range(i_max - i_min):
+      tab.append(49)
+    
+    self.tab = tab
+
+  def __len__(self) -> int:
+    return len(self.tab)
+
+  def __getitem__(self, i):
+    return self.tab[i + self.i_min]
+  
+  def __str__(self) -> str:
+    str = ""
+
+    for i in range(len(self.tab)):
+      str += f"{(i + self.i_min)} : {self.tab[i]}\n"
+
+    return str
+
+# tab = Tableau(-5, 10, 67)
+# print(tab)
+
+
+"""----------------- ex 4 -----------------"""
+class Intervalle():
+  def __init__(self, a, b) -> None:  
+    self.a = a
+    self.b = b
+
+  def __len__(self) -> int:
+    self.a - self.b
+
+  def __contains__(self, x) -> bool:
+    if self.a > x and x > self.b:
+      return True
+    return False
+  
+  def __eq__(self, i):
+    if self.a == i.a and self.b == i.b:
+      return True
+    return False
+  
+  def intersection(i, j): 
+    return Intervalle(min(i.a, j.a), max(i.b, j.b))
+  
+ 
+  def union(i, j):
+    if i.b < j.a:
+      return [i, j]
+    elif j.b < i.a:
+      return [j, i]
+    else:
+      new_a = min(i.a, j.a)
+      new_b = max(i.b, j.b)
+      return Intervalle(new_a, new_b)
+  
+
