43b648d42845abc326ab2cede5333f7f77ba1e3a
Blegourr <91756178+blegourr@users.noreply.github.com>
2024-12-16 14:20:54 +0000

    tp2 => ex2

--- TP1/ex.py
+++ TP1/ex.py
@@ -21,11 +21,38 @@ def decToBr(n,b):
     return mot
   else:
     n2 = n//b
-    mot= decToBr(n2, b) + signes[n%b]
-    return mot
+    return decToBr(n2, b) + signes[n%b]
   
 # print(decToBr(77, 2))
 # print(decToBr(77, 8))
 # print(decToBr(77, 16))
 
 """----------------- ex 2 -----------------"""
+def bToDec(mot, b):
+  assert (b>1 and b<17)
+  assert (type(mot) == str)
+  signes=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+  result=0
+  p=len(mot)
+  for i in range(p):
+    result=result+signes.index(mot[i])*b**(p-i-1)
+  return result
+
+print(bToDec('1001101', 2))
+print(bToDec('4D', 16))
+
+def bToDecR(mot, b):
+    assert (b > 1 and b < 17)
+    assert (type(mot) == str)
+    signes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+
+    if len(mot) == 1:
+        return signes.index(mot)
+
+    return signes.index(mot[0]) * b**(len(mot) - 1) + bToDec(mot[1:], b)
+
+print(bToDecR('1001101', 2))
+print(bToDecR('4D', 16))
+
+"""----------------- ex 3 -----------------"""
+
